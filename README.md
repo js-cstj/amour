c39246ba532cb68b899f7babd823c81aea30fe8b
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2021-11-08 11:11:49 -0500

    Ajout de sons et mise à jour du readme

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
-# application-base
-Application de base pour débuter un projet
+# Exercice "Amour"
+Exemple d'utilisation de setInterval et de setTimeout
 
 ## Description
 Description à venir
 
-## Particularités
+## Étapes
+1. Ajouter un coeur dans la scène à une position aléatoire (avec transform)
+    1. Ajouter un coeur dans le coin supérieur gauche
+    1. Récupérer la taille de la scène
+    1. Créer la fonction ptAlea
+    1. Placer le coeur à la position aléatoire avec le transform
+1. Placer plusieurs coeurs
+    1. À l'aide d'une boucle
+    1. À l'aide de setInterval (interval régulier)
+    1. À l'aide de setTimeout (interval variable)
+1. Faire démarrer et arrêter la création de coeurs lors du clic dans le body
+1. Faire déplacer les coeurs lors de la création
+    1. Garder en mémoire la position de départ du coeur
+    1. Déterminer une vitesse aléatoire pour chaque coeur lors de leur création (en pixels par secondes)
+    1. Déterminer l'heure du début du mouvement
+    1. Ajouter un setInterval pour calculer la position en fonction du temps et placer le coeur au bon endroit.
+1. Faire disparaître le coeur après un certain temps (aléatoire)
+    1. Créer la fonction valeurRange
+    1. Ajouter un setTimout déterminant la durée de vie d'un coeur
+    1. À échéance, ajouter la classe "disparaitre" au coeur
+    1. Ajouter un événement qui s'exécute à la fin de l'animation ("animationend") qui :
+        1. Supprime le coeur
+        1. Enlève le setTimeout de déplacement
 
-- Compléter
+1. Faire "exploser" un coeur lorsqu'il sort de l'écran
+    1. Valider la position du coeur à chaque déplacement
+    1. Supprimer le déplacement à l'aide de clearInterval
+1. Faire "exploser" un coeur lorsque la souris le touche
+    1. Ajouter un événement "mousemove" ou "mouseenter"
+    1. Enlever le coeur de l'écran
+    1. Enlever le setInterval de déplacement
+    1. Enlever le setTimeout de durée de vie
+1. Ajouter les effets sonores
+
 
 ## Variantes et ajouts possibles
 
--- src/App.js
+++ src/App.js
@@ -7,49 +7,105 @@ export default class App {
 	 */
 	static main() {
 		var app = document.getElementById("app");
-		var scene = {
-			largeur: window.innerWidth,
-			hauteur: window.innerHeight
-		};
-		window.setInterval(() => {
-			var coeur = app.appendChild(document.createElement("div"));
-			coeur.classList.add("coeur");
-			coeur.classList.add("apparaitre");
-			var image = coeur.appendChild(document.createElement("img"));
-			image.src = "images/coeur.png";
-			var pt = this.ptAlea(scene);
-			coeur.style.transform = "translate("+pt.x+"px, "+pt.y+"px)";
-			// var taille = Math.random() * .5 + .5;
-			// var angle = Math.floor(Math.random() * 60) - 30;
-			// image.style.transform = "scale("+taille+") rotate("+angle+"deg)";
-			coeur.debut = new Date().getTime();
-			coeur.depart = pt;
-			coeur.vitesse = {x: Math.floor(Math.random()*600) - 300, y: Math.floor(Math.random()*600) - 300};
-			coeur.interval = window.setInterval((obj) => {
-				var x = obj.depart.x + obj.vitesse.x * (new Date().getTime() - obj.debut) / 1000;
-				var y = obj.depart.y + obj.vitesse.y * (new Date().getTime() - obj.debut) / 1000;
-				obj.style.transform = "translate("+x+"px, "+y+"px)";
-				if (x < 0 || x > window.innerWidth || y < 0 || y > window.innerHeight) {
-					obj.remove();
+		this.actif = false;
+		this.vitesseDApparition = {min: 0, max: 1000};
+		this.dureeDeVie = {min: 1000, max: 3000};
+		this.vitesse = {x: {min: -300, max: 300}, y: {min: -300, max: 300}};
+		this.sons = [
+			"sons/QKTA234-pop.mp3",
+			"sons/sfx-pop.mp3"
+		];
+
+		document.body.addEventListener("click", e => {
+			this.actif = !this.actif;
+			if (this.actif) {
+				var scene = {
+					largeur: window.innerWidth,
+					hauteur: window.innerHeight
+				};
+				// window.setInterval(() => {
+				// 	var coeur = app.appendChild(this.html_coeur(scene));
+				// }, 100);
+				this.ajouterCoeur(scene);
+				app.style.backgroundColor = "green";
+			} else {
+				app.style.backgroundColor = "red";
+			}
+		})
+	}
+	static ajouterCoeur(scene) {
+		var coeur = app.appendChild(this.html_coeur(scene));
+		if (this.actif) {
+			window.setTimeout(() => {
+				this.ajouterCoeur(scene);
+			}, this.valeurRange(this.vitesseDApparition));
+		}
+
+	}
+	static valeurRange(range) {
+		var resultat = range;
+		if (typeof resultat === "object") {
+			resultat = Math.floor(Math.random() * (resultat.max - resultat.min + 1)) + resultat.min;
+		}
+		return resultat;
+	}
+	static html_coeur(scene) {
+		var coeur = document.createElement("div");
+		coeur.classList.add("coeur");
+		coeur.classList.add("apparaitre");
+		var image = coeur.appendChild(document.createElement("img"));
+		image.src = "images/coeur.png";
+		var pt = this.ptAlea(scene);
+		coeur.style.transform = "translate("+pt.x+"px, "+pt.y+"px)";
+		// var taille = Math.random() * .5 + .5;
+		// var angle = Math.floor(Math.random() * 60) - 30;
+		// image.style.transform = "scale("+taille+") rotate("+angle+"deg)";
+		coeur.debut = new Date().getTime();
+		coeur.depart = pt;
+		coeur.vitesse = {x: this.valeurRange(this.vitesse.x), y: this.valeurRange(this.vitesse.y)};
+		coeur.interval = window.setInterval((obj) => {
+			if (obj.classList.contains("disparaitre")) {
+				return;
+			}
+			var x = obj.depart.x + obj.vitesse.x * (new Date().getTime() - obj.debut) / 1000;
+			var y = obj.depart.y + obj.vitesse.y * (new Date().getTime() - obj.debut) / 1000;
+			obj.style.transform = "translate("+x+"px, "+y+"px)";
+			if (x < 0 || x > window.innerWidth || y < 0 || y > window.innerHeight) {
+				obj.remove();
+				window.clearInterval(obj.interval);
+				window.clearTimeout(obj.timeout);
+				this.html_pop();
+			}
+		}, 33, coeur);
+		coeur.timeout = window.setTimeout((obj) => {
+			obj.classList.remove("apparaitre");
+			obj.classList.add("disparaitre");
+			var swoosh = new Audio();
+			swoosh.src = "sons/sfx-swoosh19.mp3";
+			swoosh.play();
+			window.clearTimeout(obj.timeout);
+			obj.addEventListener("animationend", e => {
+				if (e.animationName === "disparaitre") {
 					window.clearInterval(obj.interval);
+					obj.remove();
 				}
-			}, 33, coeur);
-			window.setTimeout((obj) => {
-				obj.classList.remove("apparaitre");
-				obj.classList.add("disparaitre");
-				obj.addEventListener("animationend", e => {
-					console.log(e.animationName);
-					if (e.animationName === "apparaitre") {
-						window.clearInterval(obj.interval);
-						obj.remove();
-					}
-					if (e.animationName === "disparaitre") {
-						window.clearInterval(obj.interval);
-						obj.remove();
-					}
-				});
-			}, 3000, coeur);
-		}, 100);
+			});
+		}, this.valeurRange(this.dureeDeVie), coeur);
+		coeur.addEventListener("mousemove", e => {
+			e.currentTarget.remove();
+			window.clearInterval(e.currentTarget.interval);
+			window.clearTimeout(e.currentTarget.timeout);
+			this.html_pop();
+		});
+		return coeur;
+	}
+	static html_pop() {
+		var resultat = new Audio();
+		resultat.src = this.piger(this.sons);
+		resultat.play();
+	}
+	static piger(t) {
+		return t[Math.floor(Math.random() * t.length)];
 	}
 	static ptAlea(zone) {
 		var x = Math.floor(Math.random() * zone.largeur);
